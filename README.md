fd2c8977bb6517542067ed55e4a33338bed11505
10305 <sunrise10305@SunrisedeMacBook-Air.local>
2023-01-18 17:27:01 +0800

    init proj

---
+++ README.md
@@ -0,0 +1,5 @@
+# SnapKitDemo
+
+SnapKit is a 3rd party library which can reduce massive code during setting auto-layout constraints in hard code.
+
+Check how it works together!
---
+++ SnapKitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapKitDemo
+//
+//  Created by 10305 on 2023/1/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
